2f5c4717894396fe29ab974cc9c4bde30183a8c4
Machadows87 <machadows87@gmail.com>
2026-05-18 18:24:13 -0300

    Removendo sln da raiz para testar build limpo

--- obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aff11931a8a76e143284382e8aa6f8933bb408b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd587b03e1e2692d16a544c94f4c49ede6dee1e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
